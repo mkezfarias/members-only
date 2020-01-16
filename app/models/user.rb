bb022176ca32e933932d5e03aa0ba11ba587aecf
camfilho <camfilho@ufrn.edu.br>
2020-01-16 17:02:02 -0300

    add remember digest to creation of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
+  before_create :assign_token
   has_secure_password
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    Digest::SHA1.hexdigest(string).to_s
+  end
+
+  private
+
+  def assign_token
+    self.remember_digest = User.digest(User.new_token)
+  end
 end
