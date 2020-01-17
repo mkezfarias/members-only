0891f244ac93f149f9ada0e41ccf3793ce0db5f2
Luis Puente <puente.luism@gmail.com>
2020-01-17 06:56:36 +0000

    Add front end magic with bulma

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  #before_action :logged_in_user, only: [:new, :create]
+  before_action :logged_in_user, only: [:new, :create]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
-
+  
 
 end
