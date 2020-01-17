8ab8ba641cde9c4b580a068f37b3f8c0edd01dfc
camfilho <camfilho@ufrn.edu.br>
2020-01-17 18:29:50 -0300

    add signup and user contrl

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'posts#index'
   resources :posts, only: [:new, :create, :index, :show]
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
