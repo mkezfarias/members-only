43a7f8b4c30dc4276263f9ae76ab3ee3889f3201
camfilho <camfilho@ufrn.edu.br>
2020-01-17 18:39:32 -0300

    fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,12 @@ class User < ApplicationRecord
   end
 
   def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+
     BCrypt::Password.create(string, cost: cost)
   end
 
